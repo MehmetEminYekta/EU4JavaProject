321b5c5b1adae5fda9afc05c12e32abb119b0204
MehmetEminYekta <mhmtmn.14zg@gmail.com>
2021-01-29 13:03:15 +0100

    PROJECT is already in remote repo

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -2,4 +2,11 @@ package com;
 
 public class Hello {
     //more stuff
+    //Mac user and windows users are happy to see commit dialog finally
+
+    //I have already published on GitHub.com
+    //now I am ready to make more commits
+    //but do I need to share again????
+    //ABSOLUTELY NO
+    //IT'S TIME TO COMMIT AND JUST PUSH!!!!!!!
 }
